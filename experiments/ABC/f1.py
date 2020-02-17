383e0772acb8186be5f4bc9091c644d5d1ee5102
a2htray <a2htray.yuen@gmail.com>
2020-02-17 10:00:48 +0800

    'reconstruct'

---
+++ experiments/ABC/f1.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+_np = 60
+n = 10
+upperxs = np.array([100] * n)
+lowerxs = np.array([-100] * n)
+
+print('Global settings')
+print('Number of the population: %d' % _np)
+print('upper boundary: ', upperxs)
+print('lower boundary: ', upperxs)
